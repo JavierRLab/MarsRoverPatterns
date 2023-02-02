f6dfbf40dd5f264a5e2674cfe416f24e6f5aaeeb
MattW <matthew.warnock@codurance.com>
2023-02-02 11:03:13 +0000

    Refactor to direction

--- MarsRoverPatterns/MarsRover.cs
+++ MarsRoverPatterns/MarsRover.cs
@@ -2,12 +2,13 @@
 
 public class MarsRover
 {
+    private string compassDirection = "N";
+
     public string execute(string command)
     {
-        if(command.Equals("R")) return "0:0:E";
-        if(command.Equals("RRR")) return "0:0:W";
+        if(command.Equals("R")) compassDirection = "E";
+        if(command.Equals("RRR")) compassDirection = "W";
 
-
-        return "0:0:N";
+        return $"0:0:{compassDirection}";
     }
 }
